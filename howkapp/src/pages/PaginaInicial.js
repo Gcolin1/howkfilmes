bb0eb9ded607db8eb9a4db77cbdc81de74bfed61
Gcolin1 <guilhermecolin09@gmail.com>
2022-10-10 21:25:15 -0300

    atualização pagina inicial

--- howkapp/src/pages/PaginaInicial.js
+++ howkapp/src/pages/PaginaInicial.js
@@ -3,6 +3,8 @@ import ButtonComp from "../components/ButtonComp"
 import "./css/PaginaInicial.css"
 import logoTopo from "../assets/logoTopo.png"
 import Planet from "../assets/planet.png"
+import ImgFilmes from "../assets/img-filmes.png"
+import ImgAnimes from "../assets/img-animes.png"
 import BotaoAmarelo from "../components/BotaoAmarelo"
 
 
@@ -22,14 +24,44 @@ function PaginaInicial(){
                 <div className="gradient-bottom"></div>
             </header>
 
-            <main>
+            <main className="site container_main">
                 <section className="container-planet">
                     <div>
-                        <img className="planet" src={Planet} />
+                        <img alt="imagem planeta" className="img" src={Planet} />
                     </div>
 
-                    <div>
+                    <div className="planet-info">
+                        <h2>Bem vindo ao HawkFilmes </h2>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem 
+                        </p>
+                    </div>
+                   
+                </section>
 
+                <section className="container-filmes">
+                    <div className="planet-info">
+                        <h2>Veja suas séries e filmes favoritos </h2>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem 
+                        </p>
+                    </div>
+                   
+                    <div>
+                        <img alt="imagem planeta" className="img" src={ImgFilmes} />
+                    </div>
+                </section>
+
+                <section className="container-Animes">
+                    <div>
+                        <img alt="imagem planeta" className="img" src={ImgAnimes} />
+                    </div>
+
+                    <div className="planet-info">
+                        <h2>Veja suas séries e filmes favoritos</h2>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem 
+                        </p>
                     </div>
                 </section>
             </main>
