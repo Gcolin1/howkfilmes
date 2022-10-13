37928dd79ec807862609fdec25e748b8f27ba782
guilherme colin <guilhermecolin09@gmail.com>
2022-10-13 16:10:59 -0300

    adicionando lista de filmes e loading

--- howkapp/src/pages/PaginaInicial.js
+++ howkapp/src/pages/PaginaInicial.js
@@ -6,10 +6,12 @@ import Planet from "../assets/planet.png"
 import ImgFilmes from "../assets/img-filmes.png"
 import ImgAnimes from "../assets/img-animes.png"
 import BotaoAmarelo from "../components/BotaoAmarelo"
+import {Form, FormGroup, Input, Label, Button} from 'reactstrap'
 
 
-function PaginaInicial(){
-    return(
+function PaginaInicial() {
+
+    return (
         <div>
             <header className="bannerInicial">
                 <div className="gradient-top"></div>
@@ -17,7 +19,7 @@ function PaginaInicial(){
                     <ButtonComp />
                 </div>
                 <div className="infoTop">
-                    <img className="logoTop" src={logoTopo} alt="logo"/>
+                    <img className="logoTop" src={logoTopo} alt="logo" />
                     <p>Séries, filmes e animes que você precisa</p>
                     <BotaoAmarelo />
                 </div>
@@ -33,20 +35,20 @@ function PaginaInicial(){
                     <div className="planet-info">
                         <h2>Bem vindo ao HawkFilmes </h2>
                         <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem 
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
                         </p>
                     </div>
-                   
+
                 </section>
 
                 <section className="container-filmes">
                     <div className="planet-info">
                         <h2>Veja suas séries e filmes favoritos </h2>
                         <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem 
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
                         </p>
                     </div>
-                   
+
                     <div>
                         <img alt="imagem planeta" className="img" src={ImgFilmes} />
                     </div>
@@ -60,37 +62,73 @@ function PaginaInicial(){
                     <div className="planet-info">
                         <h2>Veja suas séries e filmes favoritos</h2>
                         <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem 
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
                         </p>
                     </div>
                 </section>
 
-                <section className="container_form">
-                    <h2>Entre em contato conosco</h2>
+                <div className="inputs">
+                    <h3>Entre em contato conosco</h3 >
+                    <Form>
+                        <FormGroup floating>
+                            <Input
+                                className="input"
+                                id="exampleNome"
+                                name="nome"
+                                placeholder="Nome"
+                                type="text"
+                            />
+                            <Label className="label" for="examplePassword">
+                                Nome
+                            </Label>
+                        </FormGroup>
 
-                    <div>
-                    <form type="submit" method="POST">
-                        <label class="login-label">
-                            <span>nome</span>
-                            <input type="text" name="nome" class="input" />
-                        </label>
-                        <label class="login-label">
-                            <span>Email</span>
-                            <input type="email" name="email" class="input" />
-                        </label>
-                        <label class="login-label">
-                            <span>Telefone</span>
-                            <input type="tel" name="fone" class="input" />
-                        </label>
-                        <label class="login-label">
-                            <span>Assunto</span>
-                            <textarea name="mens" class="input"></textarea>
-                        </label>
-                    
-                        <button class="btn-contato" type="submit">Enviar</button>
-                    </form>
-                    </div>
-                </section>  
+                        <FormGroup floating>
+                            <Input
+                                className="input"
+                                id="exampleEmail"
+                                name="email"
+                                placeholder="Email"
+                                type="email"
+                            />
+                            <Label className="label" for="exampleEmail">
+                                Email
+                            </Label>
+                        </FormGroup>
+                        {' '}
+
+                        <FormGroup floating>
+                            <Input
+                                className="input"
+                                id="exampleCelular"
+                                name="Celular"
+                                placeholder="Celular"
+                                type="text"
+                            />
+                            <Label className="label" for="examplePassword">
+                                Celular
+                            </Label>
+                        </FormGroup>
+
+                        <FormGroup floating>
+                            <Input
+                                className="input"
+                                id="exampleAssunto"
+                                name="assunto"
+                                placeholder="Assunto"
+                                type="text"
+                            />
+                            <Label className="label" for="examplePassword">
+                                Assunto
+                            </Label>
+                        </FormGroup>
+                        {' '}
+                        <Button className="botaoEnviar">
+                            Enviar
+                        </Button>
+                    </Form>
+
+                </div>
             </main>
         </div>
     )
