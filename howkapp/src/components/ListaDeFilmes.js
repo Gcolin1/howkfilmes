14d273a22da80868774a3589a036ff0e652eb501
guilherme colin <guilhermecolin09@gmail.com>
2022-10-13 12:36:37 -0300

    estilização das listas

--- howkapp/src/components/ListaDeFilmes.js
+++ howkapp/src/components/ListaDeFilmes.js
@@ -3,12 +3,14 @@ import './ListaDeFilmes.css'
 
 export default({title, items}) => {
     return(
-        <div>
-            <h2>{title}</h2>
-            <div className="LinhaDeFilmes--listarea">   
-                {items.results.length > 0 && items.results.map((item, key)=>(
-                    <img alt={items.title} src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} />
-                ))}     
+        <div className="LinhaDeFilmes">
+            <h2 className="LinhaDeFilmes--title">{title}</h2>
+            <div className="LinhaDeFilmes--container">   
+                <div class="LinhaDeFilmes--lista">
+                    {items.results.length > 0 && items.results.map((item, key)=>(
+                       <div key={key} className="LinhaDeFilmes--item"> <img alt={items.title} src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} /></div>
+                    ))}
+                </div>   
             </div>
         </div>
     )   
