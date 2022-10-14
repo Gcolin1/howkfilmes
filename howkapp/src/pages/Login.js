c9dd711c14e9247d1f7371c9e3dd6360899ec707
guilherme colin <guilhermecolin09@gmail.com>
2022-10-14 19:03:57 -0300

    atualização da pag login

--- howkapp/src/pages/Login.js
+++ howkapp/src/pages/Login.js
@@ -1,11 +1,46 @@
 import React from 'react'
+import './css/Login.css'
+import {Form, FormGroup, Input, Label, Button} from 'reactstrap'
 
 function Login(){
     return(
-        <div>
-            <h1>
-                login
-            </h1>
+        <div className='backgroundLogin'>
+           <div className='containerInput'>
+                <h1>Entrar</h1>
+                <Form>
+                        <FormGroup floating>
+                            <Input
+                                className="inputLogin"
+                                id="exampleNome"
+                                name="email"
+                                placeholder="E-mail"
+                                type="email"
+                            />
+                            <Label className="labelLogin" for="exampleEmail">
+                                E-mail
+                            </Label>
+                        </FormGroup>
+
+                        <FormGroup floating>
+                            <Input
+                                className="inputLogin"
+                                id="exampleSenha"
+                                name="senha"
+                                placeholder="Senha"
+                                type="password"
+                            />
+                            <Label className="labelLogin" for="examplePassword">
+                                Senha
+                            </Label>
+                        </FormGroup>
+                        {' '}
+                </Form>
+                <a className="linkCad">Não tem acesso? assine.</a>
+
+                <Button id='BtnLogin'>
+                   Entrar
+                </Button>
+           </div>
         </div>
     )
 }
