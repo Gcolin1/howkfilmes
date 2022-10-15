2bbe5ba63b54e58389a980e91f286ed61ff0a4ef
Eriky Costa Santos eriky <eriky.costa.santos@gmai.com>
2022-10-15 13:51:27 -0300

    Teste

--- howkapp/src/pages/Filmes.js
+++ howkapp/src/pages/Filmes.js
@@ -1,37 +1,41 @@
-import React, {useEffect, useState} from "react"
-import ListaDeFilmes from "../components/ListaDeFilmes"
-import Loading from "../components/Loading"
-import Tmdb from "../Tmdb"
+import React, { useEffect, useState } from 'react';
+import ListaDeFilmes from '../components/ListaDeFilmes';
+import Loading from '../components/Loading';
+import Tmdb from '../Tmdb';
 
-function Filmes(){
-    const [movieList, setMovieList] = useState([])
+function Filmes() {
+    const [movieList, setMovieList] = useState([]);
 
     //useEffect quandop a tela for carregada ele vai executar a função que esta dentro dela
     useEffect(() => {
         const loadfilmes = async () => {
             //pegando toda a lista de filmes
 
-            let list = await Tmdb.PaginaFilmes()
+            let list = await Tmdb.PaginaFilmes();
             setMovieList(list);
-        }
+        };
 
-        loadfilmes()
-    }, [])
+        loadfilmes();
+    }, []);
 
-    return(
-        <div className='pageHome'>
-                <section className='listas'>
-                    {movieList.map((item, key) =>(
-                        <ListaDeFilmes key={key} title={item.title} items={item.items} />
-                    ))}
-                </section>
+    return (
+        <div className="pageHome">
+            <header>
+                <h1>Buceta</h1>
+            </header>
+            <section className="listas">
+                {movieList.map((item, key) => (
+                    <ListaDeFilmes
+                        key={key}
+                        title={item.title}
+                        items={item.items}
+                    />
+                ))}
+            </section>
 
-                {movieList.length <= 0 &&
-                     <Loading />    
-               }
-
+            {movieList.length <= 0 && <Loading />}
         </div>
-    )
+    );
 }
 
 export default Filmes;
