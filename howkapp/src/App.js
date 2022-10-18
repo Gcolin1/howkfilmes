881969b851a9e8554cefa6f35fd654af1abd8275
Eriky Costa Santos eriky <eriky.costa.santos@gmai.com>
2022-10-17 23:32:03 -0300

    Page Filmes

--- howkapp/src/App.js
+++ howkapp/src/App.js
@@ -1,30 +1,29 @@
-import React from "react";
-import {Route, BrowserRouter} from "react-router-dom";
-import Animes from "./pages/Animes";
-import Filmes from "./pages/Filmes";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import PaginaInicial from "./pages/PaginaInicial";
-import ParaFamilia from "./pages/ParaFamillia";
-import Series from "./pages/Series";
-import Cadastro from "./pages/Cadastro";
-
+import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+import Animes from './pages/Animes';
+import Filmes from './pages/Filmes';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import PaginaInicial from './pages/PaginaInicial';
+import ParaFamilia from './pages/ParaFamillia';
+import Series from './pages/Series';
+import Cadastro from './pages/Cadastro';
 
 function App() {
-  return (
-    <div className="App">
-        <BrowserRouter>
-          <Route exact path="/" component={Home} />
-          <Route path="/paginainicial" component={PaginaInicial} />
-          <Route path="/filmes" component={Filmes} />
-          <Route path="/series" component={Series} />
-          <Route path="/animes" component={Animes} />   
-          <Route path="/parafamilia" component={ParaFamilia} />   
-          <Route path="/login" component={Login} />   
-          <Route path="/cadastro" component={Cadastro} />   
-        </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Route exact path="/" component={Home} />
+                <Route path="/paginainicial" component={PaginaInicial} />
+                <Route path="/filmes" component={Filmes} />
+                <Route path="/series" component={Series} />
+                <Route path="/animes" component={Animes} />
+                <Route path="/parafamilia" component={ParaFamilia} />
+                <Route path="/login" component={Login} />
+                <Route path="/cadastro" component={Cadastro} />
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- howkapp/src/pages/Filmes.js
+++ howkapp/src/pages/Filmes.js
@@ -2,6 +2,12 @@ import React, { useEffect, useState } from 'react';
 import ListaDeFilmes from '../components/ListaDeFilmes';
 import Loading from '../components/Loading';
 import Tmdb from '../Tmdb';
+import logoTopo from '../assets/logoTopo.png';
+import instagram from '../assets/instagram2.png';
+import facebook from '../assets/facebook2.png';
+import twitter from '../assets/twitter.png';
+
+import './css/Filmes.css';
 
 function Filmes() {
     const [movieList, setMovieList] = useState([]);
@@ -20,9 +26,50 @@ function Filmes() {
 
     return (
         <div className="pageHome">
-            <header>
-                <h1>Buceta</h1>
+            <header className="cabeça">
+                <img className="logoTop" src={logoTopo} alt="logo" />
+
+                <a className="navegaçao" href="https://www.youtube.com/">
+                    Filmes
+                </a>
+
+                <a className="navegaçao" href="https://www.youtube.com/">
+                    Séries
+                </a>
+
+                <a className="navegaçao" href="https://www.youtube.com/">
+                    Animes
+                </a>
+
+                <a className="navegaçao" href="https://www.youtube.com/">
+                    Para toda família
+                </a>
+
+                <div className="search-box">
+                    <input
+                        autocomplete="off"
+                        type="text"
+                        name="tPesquisar"
+                        id="Filmes"
+                        required="required"
+                        placeholder="Pesquisar..."
+                    />
+                </div>
             </header>
+
+            <section className="bem-vindo">
+                <h1>Bem-vindo ao HowkFilmes</h1>
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry.{' '}
+                </p>
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry.{' '}
+                </p>
+            </section>
+
+            <section></section>
             <section className="listas">
                 {movieList.map((item, key) => (
                     <ListaDeFilmes
@@ -34,6 +81,52 @@ function Filmes() {
             </section>
 
             {movieList.length <= 0 && <Loading />}
+
+            <footer className="rodape">
+                <img className="logoBottom" src={logoTopo} alt="logoTopo" />
+                <br></br>
+
+                <a
+                    className="logoBottom"
+                    href="https://www.instagram.com/"
+                    target="_blank_"
+                >
+                    <img
+                        className="redessocial-1"
+                        src={instagram}
+                        alt="instagram"
+                    />
+                </a>
+                <a
+                    className="logoBottom"
+                    href="https://www.facebook.com/"
+                    target="_blank_"
+                >
+                    <img
+                        className="redessocial-2"
+                        src={facebook}
+                        alt="facebook"
+                    />
+                </a>
+
+                <a
+                    className="logoBottom"
+                    href="https://twitter.com/home"
+                    target="_blank_"
+                >
+                    <img
+                        className="redessocial-3"
+                        src={twitter}
+                        alt="twitter"
+                    />
+                </a>
+                <br></br>
+
+                <small className="copy">
+                    <sup>&copy;</sup> HowkFilmes 2022 - Todos os direitos
+                    reservados.
+                </small>
+            </footer>
         </div>
     );
 }
