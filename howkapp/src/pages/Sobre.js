735b6d764aa611ddb9985f50e4860f95908895c8
EriSanto <eriky.costa.santos@gmail.com>
2022-11-10 20:41:13 -0300

    Sobre nós Objetivos

--- howkapp/src/pages/Sobre.js
+++ howkapp/src/pages/Sobre.js
@@ -10,7 +10,7 @@ function Sobre(){
                 <div id='info_sobre'>
                     <h1 className='titulo_sobre'>Sobre<span>nós</span></h1>
                     <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining.
+                    Nosso objetivo é criar o streaming que tenha a maior variação de filmes, séries, desenhos e animes do mundo. Queremos que sua experiência quando estiver procurando algo para assistir esteja em apenas um site, somos práticos, tudo que você quer ver, está aqui!
                     </p>
                 </div>
                 <div className='imgSobre'>
