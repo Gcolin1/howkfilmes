99ff9587769a72fb67797c2f4a1ae5cf6a2638d1
Gcolin1 <guilhermecolin09@gmail.com>
2022-11-10 20:34:54 -0300

    criando pagina sobre

--- howkapp/src/pages/Sobre.js
+++ howkapp/src/pages/Sobre.js
@@ -1,5 +1,8 @@
 import Menu from '../components/Menu'
 import ImgSobre from "../assets/imgSobre.png"
+import Missao from "../assets/missao.png"
+import Visao from "../assets/visao.png"
+import Valores from "../assets/valores.png"
 import './css/Sobre.css'
 
 function Sobre(){
@@ -19,7 +22,24 @@ function Sobre(){
             </section>
 
             <section id='parallaxSobre'>
-        
+
+            </section>
+
+            <section id='containerMissao'>
+                <div className="item" >
+                    <img src={Missao} alt="missão"/>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry typesetting, remaining.</p>
+                </div>
+
+                <div className="item">
+                    <img src={Visao} alt="visão"/>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry typesetting, remaining.</p>
+                </div>
+
+                <div className="item">
+                    <img src={Valores} alt="valores"/>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry typesetting, remaining.</p>
+                </div>
             </section>
         </div>
     )
