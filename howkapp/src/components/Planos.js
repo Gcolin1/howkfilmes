53089766aec5995f0220553f5302d19fb52fd672
Eriky Costa Santos eriky <eriky.costa.santos@gmai.com>
2022-11-12 16:30:04 -0300

    Planos finalizado

--- howkapp/src/components/Planos.js
+++ howkapp/src/components/Planos.js
@@ -1,40 +1,79 @@
 import React from 'react';
-import '../components/Planos.css'
+import '../components/Planos.css';
+import BotaoRoxo from '../components/BotaoRoxo'
+import { Link } from 'react-router-dom';
 
 function Planos() {
     return (
+        
+        <section>
         <div>
-            <h2 className='titulo'>Conheça nossos planos</h2>
+            <h2 className="titulo">Conheça nossos planos</h2>
 
-            <div className='planos' >
-
-                <div className='plano-1'>
-                    <p>por apenas
-                        R$20,00
-                        mensais
-                    </p>
+            <div className="planos">
+                <div className="plano-1">
+                    <p>Por apenas</p>
+                    <p id="p-1">R$20,00</p>
+                    <p>mensais</p>
                 </div>
 
-
-                <div className='plano-2'>
-                    <p>Plano anual
-                        R$156,00
-                        desconto especial
-                    </p>
+                <div className="plano-2">
+                    <p>Plano anual</p>
+                    <p id="p-2">R$156,00</p>
+                    <p>desconto especial</p>
                 </div>
 
-
-                <div className='plano-3'>
-                    <p>baixa renda
-                        pague R$12,00 e
-                        ganhe 3 messes
+                <div className="plano-3">
+                    <p>
+                        Baixa renda<br></br>
+                        pague R$12,00 e<br></br>
+                        ganhe 3 messes<br></br>
                         gratuitos
                     </p>
                 </div>
 
+                <div className="hover-1">
+                <p className='texto'>
+                    tenha acesso a nossa<br></br>
+                    plataforma e assista<br></br>
+                    seus conteudos<br></br>
+                    preferidos<br></br>
+                    <Link to={'/cadastro'}>
+                        <BotaoRoxo />
+                    </Link>
+                </p>
             </div>
+
+            <div className="hover-2">
+                <p className='texto'>
+                    tenha acesso a nossa<br></br>
+                    plataforma e assista<br></br>
+                    seus conteudos<br></br>
+                    preferidos<br></br>
+                    <Link to={'/cadastro'}>
+                        <BotaoRoxo />
+                    </Link>
+                </p>
+            </div>
+
+            <div className="hover-3">
+                <p className='texto'>
+                    tenha acesso a nossa<br></br>
+                    plataforma e assista<br></br>
+                    seus conteudos<br></br>
+                    preferidos<br></br>
+                    <Link to={'/cadastro'}>
+                        <BotaoRoxo />
+                    </Link>
+                </p>
+            </div>
+            </div>
+
+          
         </div>
-    )
+    </section>
+
+    );
 }
 
-export default Planos
+export default Planos;
