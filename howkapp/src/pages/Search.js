c966178226e9cb2873bca2ca07e990c42fab11d6
Gcolin1 <guilhermecolin09@gmail.com>
2022-11-22 18:48:50 -0300

    atualização search

--- howkapp/src/pages/Search.js
+++ howkapp/src/pages/Search.js
@@ -3,11 +3,11 @@ import { useLocation } from 'react-router-dom';
 import { useEffect, useMemo } from 'react';
 import { useState } from 'react';
 import './css/Search.css';
-import MovieCard from '../components/MovieCard'
+import MovieCard from '../components/MovieCard';
 
 function Search() {
     const API_KEY = '582270ca0cc6eefd08e343e5c968c71f';
-    const searchUrl = "https://api.themoviedb.org/3/search/movie/"
+    const searchUrl = 'https://api.themoviedb.org/3/search/movie/';
     const { search } = useLocation();
 
     const searchParams = useMemo(() => new URLSearchParams(search), [search]);
@@ -18,14 +18,14 @@ function Search() {
     const getSearchMovies = async (url) => {
         const req = await fetch(url);
         const json = await req.json();
-        console.log(movies)
         setMovies(json.results);
+        console.log(movies);
     };
 
     useEffect(() => {
-        const searchWithQueryURL = `${searchUrl}?api_key=${API_KEY}&query=${query}`
+        const searchWithQueryURL = `${searchUrl}?api_key=${API_KEY}&query=${query}`;
 
-        getSearchMovies(searchWithQueryURL)
+        getSearchMovies(searchWithQueryURL);
     }, [query]);
 
     return (
@@ -34,7 +34,20 @@ function Search() {
             <h2 className="query-text">
                 Resultados para: <span>{query}</span>
             </h2>
-            
+            {movies.length <= 0 &&
+                    <p>Carregando...</p>
+            }
+            <div className='container_movies'>
+                {movies.map((movie) => (
+                    <div className='item_movie' key={movie}>
+                        <img
+                            alt={movie.title}
+                            src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+                            className="movie_img"
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
