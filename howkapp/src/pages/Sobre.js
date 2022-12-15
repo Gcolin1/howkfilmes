efe476b50ae048fc65371de6c9df71e024e40f26
Gcolin1 <guilhermecolin09@gmail.com>
2022-12-15 18:57:22 -0300

    atualização sobre

--- howkapp/src/pages/Sobre.js
+++ howkapp/src/pages/Sobre.js
@@ -142,7 +142,7 @@ function Sobre() {
                         <a
                             className="linkLinkedin"
                             target="_blank"
-                            href="https://www.linkedin.com/in/guilhermevarjãosantos" rel="noreferrer"
+                            href="https://www.linkedin.com/in/" rel="noreferrer"
                         >
                             linkedin
                         </a>
